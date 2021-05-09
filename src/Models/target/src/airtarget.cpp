c19d71f5b1226d2d8962a71649e644b97ca8cc7b
lex268 <morozov.ao@phystech.edu>
2021-05-09 14:57:45 +0300

    Продолжаю сшивать.

--- src/Models/target/src/airtarget.cpp
+++ src/Models/target/src/airtarget.cpp
@@ -56,10 +56,10 @@ void AirTarget::step(double time)
         }
     }
     if (status == TargetStatus::is_fly)
-        calculate(1);
+        calculate();
 }
 
-void AirTarget::calculate(int num_iter)
+void AirTarget::calculate()
 {
     std::vector<Vector3D> _points;
     std::vector<Vector3D> _vels;
@@ -70,224 +70,219 @@ void AirTarget::calculate(int num_iter)
     std::vector<double> _horizontal_plane;
     std::vector<double> _wayAngle;
 
-    Vector3D NuCur, NuCurDer;
-    Vector3D NuDes, NuDesDer;
-    Vector3D NuCurNext;
+    Vector3D NuCur(0,0,0);
+    Vector3D NuCurDer(0,0,0);
+    Vector3D NuDes(0,0,0);
+    Vector3D NuDesDer(0,0,0);
+    Vector3D NuCurNext(0,0,0);
     Vector3D radiusVector(0,0,0);
-    Vector3D NormalOverload, VelComp, AccelsComp;
+    Vector3D NormalOverload(0,0,0);
+    Vector3D VelComp(0,0,0);
+    Vector3D AccelsComp(0,0,0);
 
     double k_nu = 0.6;
     double fi = 0;
-    double dt = 0.1;
 
-    bool first_time = true;
+    int i = num_point_passed;
+    GeocentricCoodinates GC_1;
 
-    for (int i = num_point_passed; i < control_points.size()-1; i++, num_point_passed++)
+    if (data.nPoints == 0)
     {
-        double speed = control_points[i].initialVel;
-        int iter = 0;
-        GeocentricCoodinates GC_1(data.xPos.back(), data.yPos.back(), data.zPos.back());
-        GeocentricCoodinates GC_2(control_points[i+1].initialPoint.x, control_points[i+1].initialPoint.y, control_points[i+1].initialPoint.z);
-        GeocentricCoodinates GC = GC_1;
-        GeodezicCoodinates GD = GeocentricToGeodezic(GC_1).toRadians();
+        GC_1 = GeocentricCoodinates(control_points[0].initialPoint.x, control_points[0].initialPoint.y, control_points[0].initialPoint.z);
+        _points.push_back(Vector3D(GC_1.x, GC_1.y, GC_1.z));
+        _vels.push_back({0,0,0});
+        _accels.push_back(AccelsComp);
+        _normOV.push_back(0);
+        _transOV.push_back(0);
+        _horizontal_plane.push_back(0);
+        _bankAngle.push_back(0);
+        _wayAngle.push_back(0);
+    }
+    else
+    {
+        GC_1 = GeocentricCoodinates(data.xPos.back(), data.yPos.back(), data.zPos.back());
+        double vel = sqrt(data.xVel.back() * data.xVel.back() + data.yVel.back() * data.yVel.back()
+                              + data.zVel.back() * data.zVel.back());
 
-        iter++;
-        k_nu = 0.6;
-        Vector3D LocalCoord_2 = GeoToLocal(GD, GC_2, GC);
+        NuCur = {data.xVel.back()/vel, data.yVel.back()/vel, data.zVel.back()/vel};
+        NuCur = this->NuCurNext;
 
-        NuCur.x = NuCurLast[0];
-        NuCur.y = NuCurLast[1];
-        NuCur.z = NuCurLast[2];
-        _vels.push_back(Vector3D(NuCur*speed));
-        _accels.push_back(Vector3D(AccelsLast[0], AccelsLast[1], AccelsLast[2]));
-        _normOV.push_back(NormLast);
-        _transOV.push_back(TransLast);
-        _bankAngle.push_back(BankLast);
-        _horizontal_plane.push_back(HorizLast);
-        _wayAngle.push_back(WayLast);
+        _vels.push_back(Vector3D(NuCur*vel));
 
-        if (NuCur.length() != 0)
-            first_time = false;
+        _accels.push_back(Vector3D(data.xAcc.back(), data.yAcc.back(), data.zAcc.back()));
+        _normOV.push_back(data.normalOverload.back());
+        _transOV.push_back(data.transverseOverload.back());
+        _horizontal_plane.push_back(data.angle_horizontal_plane.back());
+        _bankAngle.push_back(data.bankAngle.back());
+        _wayAngle.push_back(data.wayAngle.back());
+    }
 
-        NuDes = (LocalCoord_2 - radiusVector);
-        NuDes.normalize();
+    double speed = control_points[i].initialVel;
+    GeocentricCoodinates GC_2(control_points[i+1].initialPoint.x, control_points[i+1].initialPoint.y, control_points[i+1].initialPoint.z);
+    GeocentricCoodinates GC = GC_1;
+    GeodezicCoodinates GD = GeocentricToGeodezic(GC_1).toRadians();
 
-        double fi_for_correction = angeleBetween(Vector3D(GC), Vector3D(GC_2));
-        double speedForChangingHeight = (Vector3D(GC_2).length() - Vector3D(GC).length()) / fi_for_correction;
-        double alphaCorrection = acos ( sqrt ( Vector3D(GC).sqrlength() /
-                                               (Vector3D(GC).sqrlength() + speedForChangingHeight * speedForChangingHeight)));
-        double B = 0;
-        if (pow(NuDes.x, 2) + pow(NuDes.z, 2) > 0.0001)
-            B = sqrt (cos(alphaCorrection) / (pow(NuDes.x, 2) + pow(NuDes.z, 2)));
-        else
-            B = 0;
+    k_nu = 0.6;
+    Vector3D LocalCoord_2 = GeoToLocal(GD, GC_2, GC);
 
-        NuDes.x *= B;
-        NuDes.z *= B;
-        if (fabs(Vector3D(GC_2).length() - Vector3D(GC).length()) > 0.00001)
-            NuDes.y = sin(alphaCorrection) * Sign(Vector3D(GC_2).length() - Vector3D(GC).length());
-        else
-            NuDes.y = 0;
+    NuDes = (LocalCoord_2 - radiusVector);
+    NuDes.normalize();
 
-        NuDes.normalize();
+    double fi_for_correction = angeleBetween(Vector3D(GC), Vector3D(GC_2));
+    double speedForChangingHeight = (Vector3D(GC_2).length() - Vector3D(GC).length()) / fi_for_correction;
+    double alphaCorrection = acos ( sqrt ( Vector3D(GC).sqrlength() /
+                                           (Vector3D(GC).sqrlength() + speedForChangingHeight * speedForChangingHeight)));
+    double B = 0;
+    if (pow(NuDes.x, 2) + pow(NuDes.z, 2) > 0.0001)
+        B = sqrt (cos(alphaCorrection) / (pow(NuDes.x, 2) + pow(NuDes.z, 2)));
+    else
+        B = 0;
 
-        if (i == 0 && first_time)
+    NuDes.x *= B;
+    NuDes.z *= B;
+    if (fabs(Vector3D(GC_2).length() - Vector3D(GC).length()) > 0.00001)
+        NuDes.y = sin(alphaCorrection) * Sign(Vector3D(GC_2).length() - Vector3D(GC).length());
+    else
+        NuDes.y = 0;
+
+    NuDes.normalize();
+
+    if (data.nPoints == 0)
+    {
+        NuCur = NuDes;
+    }
+
+    double cosFi = dot(NuCur, NuDes);
+
+    if (cosFi >= 0.9999999999)
+    {
+        fi = 0;
+        cosFi = 1;
+        NuDesDer(0, 0, 0);
+
+        //                if (k_nu - 0.3*dt*dt > 0)
+        //                    k_nu -= 0.3*dt*dt;
+        //                else
+        //                    k_nu = 0;
+    }
+    else if (cosFi <= -0.9999999999)
+    {
+        double alpha =  M_PI/30;
+        double x = NuCur.x;
+        double z = NuCur.z;
+
+        NuCur.x = x*cos(alpha) - z*sin(alpha);
+        NuCur.z = x*sin(alpha) + z*cos(alpha);
+
+        cosFi = dot(NuCur, NuDes);
+        fi = acos(cosFi);
+
+        //                if (k_nu < 0.3)
+        //                    k_nu += 0.3*dt*dt;
+        NuDesDer = (NuDes - NuCur*cosFi)*(k_nu*fi/sin(fi));
+    }
+    else
+    {
+        fi = acos(cosFi);
+
+        //                if (k_nu < 0.3)
+        //                    k_nu += 0.3*dt*dt*dt;
+        NuDesDer = (NuDes - NuCur*cosFi)*(k_nu*fi/sin(fi));
+    }
+
+    //std::cout << "NuCur " << NuCur.x << " " << NuCur.y << " " << NuCur.z << " " << "NuDes " << NuDes.x  << " " << NuDes.y << " " << NuDes.z << "\n";
+
+    Vector3D OY(0, 1, 0);
+    NormalOverload = NuDesDer*(speed/g) + OY - NuCur*NuCur.y;
+
+    if (param.MAX_NORMAL_OVERLOAD > 0)
+    {
+        if (NormalOverload.length() >= param.MAX_NORMAL_OVERLOAD)
         {
-            NuCur = NuDes;
-            first_time = false;
+            Vector3D B = (NuDes - NuCur*cosFi)*(speed*fi/(g*sin(fi)));
+            double a = B.sqrlength();
+            double b = B.y - dot(B, NuCur)*NuCur.y;
+            double c = 1 - NuCur.y*NuCur.y - param.MAX_NORMAL_OVERLOAD*param.MAX_NORMAL_OVERLOAD;
+
+            if (a == 0)
+                k_nu = -1*c / b;
+            else
+                k_nu = (- b + sqrt( b * b - a * c )) / a;
+
+            if (fi != 0)
+                NuDesDer = (NuDes - NuCur*cosFi)*(k_nu*fi/sin(fi));
+            else
+                NuDesDer = (NuDes - NuCur*cosFi)*k_nu;
+
+            NormalOverload = NuDesDer*(speed/g) + OY - NuCur*NuCur.y;
         }
+    }
 
-        double cosFi = dot(NuCur, NuDes);
+    _normOV.push_back(NormalOverload.length());
 
-        if (cosFi >= 0.9999999999)
-        {
-            fi = 0;
-            cosFi = 1;
-            NuDesDer(0, 0, 0);
+    NuCurDer = (NormalOverload - OY + NuCur*NuCur.y) * (g/speed);
 
-            //                if (k_nu - 0.3*dt*dt > 0)
-            //                    k_nu -= 0.3*dt*dt;
-            //                else
-            //                    k_nu = 0;
-        }
-        else if (cosFi <= -0.9999999999)
-        {
-            double alpha =  M_PI/30;
-            double x = NuCur.x;
-            double z = NuCur.z;
+    int k = 0;
+    if (speed < control_points[i+1].initialVel && speed <= param.MAX_MAH*330)
+    {
+        k++;
+        speed += g*param.MAX_TRANSVERSE_OVERLOAD*dt;
+        _transOV.push_back(param.MAX_TRANSVERSE_OVERLOAD);
+    }
 
-            NuCur.x = x*cos(alpha) - z*sin(alpha);
-            NuCur.z = x*sin(alpha) + z*cos(alpha);
+    if (speed > control_points[i+1].initialVel && speed <= param.MAX_MAH*330)
+    {
+        k++;
+        speed += g*param.MIN_TRANSVERSE_OVERLOAD*dt;
+        _transOV.push_back(param.MIN_TRANSVERSE_OVERLOAD);
+    }
 
-            cosFi = dot(NuCur, NuDes);
-            fi = acos(cosFi);
+    if (k == 0)
+        _transOV.push_back(param.MAX_TRANSVERSE_OVERLOAD);
 
-            //                if (k_nu < 0.3)
-            //                    k_nu += 0.3*dt*dt;
-            NuDesDer = (NuDes - NuCur*cosFi)*(k_nu*fi/sin(fi));
-        }
-        else
-        {
-            fi = acos(cosFi);
+    if (speed >= param.MAX_MAH*330)
+        speed = param.MAX_MAH*330;
 
-            //                if (k_nu < 0.3)
-            //                    k_nu += 0.3*dt*dt*dt;
-            NuDesDer = (NuDes - NuCur*cosFi)*(k_nu*fi/sin(fi));
-        }
+    VelComp = NuCur*speed;
+    AccelsComp = (VelComp - _vels.back()) / dt;
+    _vels.push_back(VelComp);
+    _accels.push_back(AccelsComp);
 
-        //std::cout << "NuCur " << NuCur.x << " " << NuCur.y << " " << NuCur.z << " " << "NuDes " << NuDes.x  << " " << NuDes.y << " " << NuDes.z << "\n";
+    NuCurNext = NuCur + NuCurDer*dt;
+    NuCurNext.normalize();
 
-        Vector3D OY(0, 1, 0);
-        NormalOverload = NuDesDer*(speed/g) + OY - NuCur*NuCur.y;
+    radiusVector += NuCurNext*(speed*dt);
+    GC = LocalToGeo(GD, radiusVector, GC);
+    _points.push_back(Vector3D(GC.x, GC.y, GC.z));
 
-        if (param.MAX_NORMAL_OVERLOAD > 0)
-        {
-            if (NormalOverload.length() >= param.MAX_NORMAL_OVERLOAD)
-            {
-                Vector3D B = (NuDes - NuCur*cosFi)*(speed*fi/(g*sin(fi)));
-                double a = B.sqrlength();
-                double b = B.y - dot(B, NuCur)*NuCur.y;
-                double c = 1 - NuCur.y*NuCur.y - param.MAX_NORMAL_OVERLOAD*param.MAX_NORMAL_OVERLOAD;
+    double Lx = NormalOverload.x / NormalOverload.length();
+    double Lz = NormalOverload.z / NormalOverload.length();
+    _bankAngle.push_back(asin(-NuCur.x*Lx + NuCur.z*Lz));
 
-                if (a == 0)
-                    k_nu = -1*c / b;
-                else
-                    k_nu = (- b + sqrt( b * b - a * c )) / a;
+    _horizontal_plane.push_back(asin(NuCur.y));
+    _wayAngle.push_back(asin(NuCur.z/(cos(asin(NuCur.y)))));
 
-                if (fi != 0)
-                    NuDesDer = (NuDes - NuCur*cosFi)*(k_nu*fi/sin(fi));
-                else
-                    NuDesDer = (NuDes - NuCur*cosFi)*k_nu;
+    GD = GeocentricToGeodezic(GC).toRadians();
 
-                NormalOverload = NuDesDer*(speed/g) + OY - NuCur*NuCur.y;
-            }
-        }
+    double dist = (GC_2 - GC).length();
 
-        ///std::cout << "k cosfi fi " << k_nu << " " << cosFi << " " << fi << "\n";
-
-        _normOV.push_back(NormalOverload.length());
-
-        NuCurDer = (NormalOverload - OY + NuCur*NuCur.y) * (g/speed);
-
-        ///std::cout << "NuCurDer " << NuCurDer.x << " " << NuCurDer.y << " " << NuCurDer.z << " NuDesDer " << NuDesDer.x << " " << NuDesDer.y << " " << NuDesDer.z << "\n";
-
-        int k = 0;
-        if (speed < control_points[i+1].initialVel && speed <= param.MAX_MAH*330)
-        {
-            k++;
-            speed += g*param.MAX_TRANSVERSE_OVERLOAD*dt;
-            _transOV.push_back(param.MAX_TRANSVERSE_OVERLOAD);
-        }
-
-        if (speed > control_points[i+1].initialVel && speed <= param.MAX_MAH*330)
-        {
-            k++;
-            speed += g*param.MIN_TRANSVERSE_OVERLOAD*dt;
-            _transOV.push_back(param.MIN_TRANSVERSE_OVERLOAD);
-        }
-
-        if (k == 0)
-            _transOV.push_back(param.MAX_TRANSVERSE_OVERLOAD);
-
-        if (speed >= param.MAX_MAH*330)
-            speed = param.MAX_MAH*330;
-
+    if (dist <= 100)
+    {
+        radiusVector = GeoToLocal(GD, GC_2, GC);
+        _points.push_back(Vector3D(GC_2.x, GC_2.y, GC_2.z));
         VelComp = NuCur*speed;
         AccelsComp = (VelComp - _vels.back()) / dt;
         _vels.push_back(VelComp);
         _accels.push_back(AccelsComp);
-
-        NuCurNext = NuCur + NuCurDer*dt;
-        NuCurNext.normalize();
-
-        radiusVector += NuCurNext*(speed*dt);
-        GC = LocalToGeo(GD, radiusVector, GC);
-        _points.push_back(Vector3D(GC.x, GC.y, GC.z));
-
-        double Lx = NormalOverload.x / NormalOverload.length();
-        double Lz = NormalOverload.z / NormalOverload.length();
-        _bankAngle.push_back(asin(-NuCur.x*Lx + NuCur.z*Lz));
-
-        _horizontal_plane.push_back(asin(NuCur.y));
-        _wayAngle.push_back(asin(NuCur.z/(cos(asin(NuCur.y)))));
-
-        radiusVector(0, 0, 0);
-        GD = GeocentricToGeodezic(GC).toRadians();
-
-        double dist = (GC_2 - GC).length();
-
-        //std::cout << dist << "\n";
-
-        if (dist <= 100)
-        {
-            std::cout << "YES YES\n";
-            radiusVector = GeoToLocal(GD, GC_2, GC);
-            _points.push_back(Vector3D(GC_2.x, GC_2.y, GC_2.z));
-            VelComp = NuCur*speed;
-            AccelsComp = (VelComp - _vels.back()) / dt;
-            _vels.push_back(VelComp);
-            _accels.push_back(AccelsComp);
-            _normOV.push_back(_normOV.back());
-            _transOV.push_back(_transOV.back());
-            _bankAngle.push_back(_bankAngle.back());
-            _horizontal_plane.push_back(_horizontal_plane.back());
-            _wayAngle.push_back(_wayAngle.back());
-            num_point_passed++;
-        }
-
-        NuCur = NuCurNext;
-
-        if (((iter >= num_iter) && (num_iter > 0)))
-            break;
+        _normOV.push_back(_normOV.back());
+        _transOV.push_back(_transOV.back());
+        _bankAngle.push_back(_bankAngle.back());
+        _horizontal_plane.push_back(_horizontal_plane.back());
+        _wayAngle.push_back(_wayAngle.back());
+        num_point_passed++;
     }
 
-    NuCurLast = {NuCurNext.x, NuCurNext.y, NuCurNext.z};
-    AccelsLast = {_accels.back().x, _accels.back().y, _accels.back().z};
-    NormLast = _normOV.back();
-    TransLast = _transOV.back();
-    HorizLast = _horizontal_plane.back();
-    BankLast = _bankAngle.back();
-    WayLast = _wayAngle.back();
+    this->NuCurNext = NuCurNext;
     data.insert(data.nPoints, _points, _vels, _accels, _bankAngle, _horizontal_plane, _wayAngle, _transOV, _normOV, dt);
 }
 
--- src/Models/target/src/airtarget.h
+++ src/Models/target/src/airtarget.h
@@ -8,6 +8,8 @@
 #include "msg_types.h"
 
 constexpr double g = 9.81;
+constexpr double dt = 0.1;
+
 
 struct AirTargetParams
 {
@@ -22,7 +24,7 @@ class AirTarget : public Model
 public:
     AirTarget(std::string targetName_, AirTargetParams param_);
 
-    void calculate(int num_iter);
+    void calculate();
 
     bool init(std::string const& initial_data) override final;
     void firstStep() override final;
@@ -45,17 +47,17 @@ private:
     std::vector<TrajectoryPoint> control_points;
     TrajData data;
 
-    std::vector<double> NuCurLast = {0, 0, 0};
-    std::vector<double> NuCurNextLast = {0, 0, 0};
-    std::vector<double> AccelsLast = {0, 0, 0};
-    double NormLast = 0;
-    double TransLast = 0;
-    double HorizLast = 0;
-    double BankLast = 0;
-    double WayLast = 0;
+    Vector3D NuCurNext;
+
+    GeodezicCoodinates GD_Msc = GeodezicCoodinates(55, 37, 1000); //ךמסעûûûûכü
+
+    void TransformForCalculate();
+
 };
 
 Vector3D GeoToLocal(GeodezicCoodinates GD, GeocentricCoodinates GC, GeocentricCoodinates GC0);
 GeocentricCoodinates LocalToGeo(GeodezicCoodinates GD, Vector3D Loc, GeocentricCoodinates GC0);
+Vector3D GeoToPBU(GeocentricCoodinates GC0, GeocentricCoodinates GC);
+GeocentricCoodinates PBUToGeo(GeocentricCoodinates GC0, Vector3D PBU);
 
 #endif // AIRTARGET_H
