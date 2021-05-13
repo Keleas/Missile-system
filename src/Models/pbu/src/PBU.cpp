43e74afe2ce944084530d2db97c470263530ee65
TeleshevVictor <teleshev.victor@yandex.ru>
2021-05-13 11:55:30 +0300

    Добавлена реализация ТОИ

--- src/Models/pbu/src/PBU.cpp
+++ src/Models/pbu/src/PBU.cpp
@@ -1,26 +1,104 @@
 #include "PBU.h"
 
-PBU::PBU(id_type id, MsgChannelCarrier& carrier, std::ostream& log)
-    : Model{id, carrier, log}
+PBU::PBU(id_type id, MsgChannelCarrier& carrier)
+    : Model{id, carrier}
 {
-    //declareteQueue(msg_from_rlc);
+    declareteQueue(msg_from_rlc);
 }
 
 
 PBU::~PBU() {}
 
-//bool PBU::init(const std::string &initial_data) {
-//    return true;
-//}
-
-void PBU::firstStep() {
-
+bool PBU::init(const std::string &initial_data) {
+    return true;
 }
 
+void PBU::firstStep() {
+    GetRLIfromRadar();
+    //FindIdenticalTracks();
+
+}
+//  std::map<id_type, std::map<int, RLCMsg>> all_traces;
+void PBU::GetRLIfromRadar()
+{
+    double step_time = msg_from_rlc.front().time;
+    while (!msg_from_rlc.empty())
+    {
+        if(id_table.empty())
+        {
+            void AddNewTarget();            
+        }
+        else
+        {
+            if(id_table.count(std::make_pair(msg_from_rlc.front().source_id,
+                                             msg_from_rlc.front().message.target_id)) == 0) // Если данные о цели с РЛС k еще не приходили
+            {
+                for(auto& target : targets) // Если цель была обнаружена другими РЛС
+                {
+                    if(CheckTrack(msg_from_rlc.front().message, target.second))
+                    {
+                        id_table[std::make_pair(msg_from_rlc.front().source_id,
+                                                msg_from_rlc.front().message.target_id)] = target.second.ID;
+
+                        history_id[target.second.ID].insert(std::make_pair(msg_from_rlc.front().source_id,
+                                                                           msg_from_rlc.front().message.target_id));
+
+                        targets_time[std::make_pair(msg_from_rlc.front().source_id,
+                                                    msg_from_rlc.front().message.target_id)] = msg_from_rlc.front().time;
+
+                        target.second.history_coords.push_back(msg_from_rlc.front().message.coordinates);
+                        target.second.history_speed.push_back(msg_from_rlc.front().message.speed);
+
+                        break;
+                    }
+                }
+                void AddNewTarget();    // Такой цели не было обнаружено другими РЛС
+            }
+            else                        // Инфформация об данной цели уже приходила с РЛС k
+            {
+                id_type My_ID = id_table[std::make_pair(msg_from_rlc.front().source_id,
+                                                       msg_from_rlc.front().message.target_id)];
+
+                targets_time[std::make_pair(msg_from_rlc.front().source_id,
+                                            msg_from_rlc.front().message.target_id)] = msg_from_rlc.front().time; // обновляем время
+
+
+                targets[My_ID].history_coords.push_back(msg_from_rlc.front().message.coordinates);
+                targets[My_ID].history_speed.push_back(msg_from_rlc.front().message.speed);
+            }
+        }
+        msg_from_rlc.erase(msg_from_rlc.begin());
+    }
+
+ /*******************************В этом блоке происходит поиск тех целей, которые не были обнавленны на данном шаге****************************************/
+
+    for(auto& item : targets_time) // Смотрим по времени, какие данные обновились за этот шаг
+    {
+        if(item.second != step_time)
+        {
+            std::pair<id_type, id_type> ID_from_rlc = item.first;
+            int  My_ID = id_table[item.first];
+
+            id_table.erase(ID_from_rlc);      // удаляем умоминания из таблицы соответсвия
+
+            history_id[My_ID].erase(ID_from_rlc.first);
+            if(history_id.count(My_ID) == 0)
+            {
+                targets.erase(My_ID);
+            }
+
+            targets_time.erase(item.first);
+        }
+    }
+    for(auto& item : targets)
+        item.second.CalculateParametrs();                       // Пересчитываем координаты
+}
+
+
 void PBU::step(double time) {
-//обработка сообщений
-//моделирование
-//отправка сообщений
+    //обработка сообщений
+    //моделирование
+    //отправка сообщений
 }
 
 
--- src/Models/pbu/src/PBU.h
+++ src/Models/pbu/src/PBU.h
@@ -4,21 +4,126 @@
 #include "channel_carrier.h"
 #include "abstractmodel.h"
 #include "msg_types.h"
+#include <map>
+
+
+
+
 
 class PBU : public Model
 {
 public:
-    PBU(id_type id, MsgChannelCarrier& carrier, std::ostream& log);
+    PBU(id_type id, MsgChannelCarrier& carrier);
     ~PBU();
 
-//    bool init(std::string const& initial_data) override final;
+    bool init(std::string const& initial_data) override final;
 
     void firstStep() override final;
 
     void step(double time) final;
+
+    void GetRLIfromRadar();
+
 private:
-    //MessageQueue<RLCMsg> msg_from_rlc;
+    MessageQueue<RLCMsg> msg_from_rlc;
+
+
+    int target_counter = 1;
+
+    class Target
+    {
+    public:
+        Target();
+        Target(int id, const RLCMsg& msg)
+            : coords({{msg.coordinates[0],msg.coordinates[1],msg.coordinates[2]}}),
+              speed({{msg.speed[0],msg.speed[1],msg.speed[2]}}),
+              ID(ID),
+              time(msg.time)
+        {}
+
+        //        Target& operator=(const RLCMsg& msg);
+
+        void CalculateParametrs()
+        {
+            for(size_t i = 0; i < history_coords.size(); i++)
+            {
+                for(size_t j = 0; j < 3; j++ )
+                {
+                    coords[j] += history_coords[i][j];
+                    speed[j] += history_speed[i][j];
+                }
+            }
+            for(size_t i = 0; i < history_coords.size(); i++)
+            {
+                coords[i] = (coords[i] / history_coords.size());
+                speed[i] = (speed[i] / history_speed.size());
+            }
+
+            history_coords.erase(history_coords.begin(), history_coords.end());
+            history_speed.erase(history_speed.begin(), history_speed.end());
+
+            history_coords.resize(0);
+            history_speed.resize(0);
+        }
+        //std::map<id_type, id_type> history_id; // first - id_rlc, second - id_target
+
+        std::vector<std::vector<double>> history_coords;
+        std::vector<std::vector<double>> history_speed;
+
+        std::vector<double> coords;
+        std::vector<double> speed;
+
+        id_type ID;
+        double time = 0;
+    };
+
+    bool CheckTrack(const RLCMsg& t1, const Target& t2)
+    {
+
+        return ((t1.coordinates[0] - t2.coords[0]) * (t1.coordinates[0] - t2.coords[0]) +
+                (t1.coordinates[1] - t2.coords[1]) * (t1.coordinates[1] - t2.coords[1]) +
+                (t1.coordinates[2] - t2.coords[2]) * (t1.coordinates[2] - t2.coords[2]) <= 2500) ||
+
+                ((t1.speed[0] * t1.speed[0]) - (t2.speed[0] * t2.speed[0]) +
+                (t1.speed[1] * t1.speed[1]) - (t2.speed[1] * t2.speed[1]) +
+                (t1.speed[2] * t1.speed[2]) - (t2.speed[2] * t2.speed[2]) <= 400);
+    }
+
+    void AddNewTarget()
+    {
+        id_table[std::make_pair(msg_from_rlc.front().source_id,
+                                msg_from_rlc.front().message.target_id)] = target_counter;
+
+        history_id[target_counter].insert(std::make_pair(msg_from_rlc.front().source_id,
+                                                         msg_from_rlc.front().message.target_id));
+
+        targets_time[std::make_pair(msg_from_rlc.front().source_id,
+                                    msg_from_rlc.front().message.target_id)] = msg_from_rlc.front().time;
+        targets.insert({target_counter, Target(target_counter, msg_from_rlc.front().message)});
+        ++target_counter;
+    }
+
+
+    std::map<std::pair<id_type, id_type>, int> id_table;
+    std::map<int, std::map<id_type, id_type>> history_id;
+    std::map<std::pair<id_type, id_type>, double> targets_time;
+    std::map<id_type, Target> targets;
+
+
+    //std::map<id_type, std::map<id_type, RLCMsg>> all_traces; // key - rls_id, key2 - target_id
+    //std::vector<std::vector<std::pair<int,int>>> identical_tracks; // <pair<rlc_id,target_id>, map<rlc_id,target_id>>
+
 };
+
+
+//struct RLCMsg
+//{
+//
+//    vec3 coordinates;
+//    vec3 speed; 				//v_x, v_y, v_z
+//    double time;              //временная метка
+//    double v_r;      			// скорость на лакатор
+//};
 
 
 #endif //MISSILESYSTEM_PBU_H
