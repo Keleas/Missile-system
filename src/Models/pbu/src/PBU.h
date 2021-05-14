38574d17d5e80575f2521b5e717ccff482029523
TeleshevVictor <teleshev.victor@yandex.ru>
2021-05-14 16:54:34 +0300

    Начало реализации работы с ПУ

--- src/Models/pbu/src/PBU.h
+++ src/Models/pbu/src/PBU.h
@@ -4,6 +4,7 @@
 #include "channel_carrier.h"
 #include "abstractmodel.h"
 #include "msg_types.h"
+#include <cmath>
 #include <map>
 
 class PBU : public Model
@@ -21,8 +22,12 @@ public:
     void GetRLIfromRadar();  // Третичная обработка
 
 private:
-    MessageQueue<RLCMsg> msg_from_rlc;
-    MessageQueue<RLCMsg> recieve_msg;
+    MessageQueue<RLCMsg> msg_from_rlc;                   // очередь сообщений от РЛС
+
+    MessageQueue<PUtoPBUstartMsg> msg_from_pu_start;     // очередь сообщений от ПУ (для FirstStep)
+    MessageQueue<PUtoPBUzurIDMsg> msg_from_pu;           // очередь сообщений от ПУ (обновление данных о состояниии ПУi)
+    MessageQueue<PUtoPBUstartMsg> msg_from_pu_zur;       // очередь сообщений от ПУ (получения ID ЗУР)
+
 
     class Target
     {
@@ -48,8 +53,30 @@ private:
         double time = 0;
         };
 
+    class PU{
+    public:
+        PU();
+        PU(const PUtoPBUstartMsg& msg)
+            : zur_num(msg.zur_num), status(msg.status),
+              coords({msg.coord[0], msg.coord[0], msg.coord[0]})
+        {}
+
+
+
+        int zur_num;
+        bool status;
+        std::vector<double> coords;
+
+    };
+
+    double CalculateDistanse(const std::vector<double>& a, const std::vector<double>& b);
+
     bool CheckTrack(const RLCMsg& t1, const Target& t2);
     void AddNewTarget();
+
+    void FirstStepFromPU();
+
+    void TargetDistribution();
 
 
     std::map<std::pair<id_type, id_type>, int> id_table;            // key - <first - rls_id, second - target_id>, value - My_id
@@ -58,9 +85,10 @@ private:
     std::map<id_type, Target> targets;                              // key - My_id, value - Target
     int target_counter = 1;
 
-    std::map<id_type, std::map<id_type, bool>> pu_base;
+    std::map<id_type, PU> pu_base;                                  // key - My_id, value - Target
     std::map<id_type, std::vector<double>> pu_coords;
-};
 
+    std::vector<double> pbu_coords;
+};
 
 #endif //MISSILESYSTEM_PBU_H
